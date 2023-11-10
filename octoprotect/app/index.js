29b2908e9ef5b9f73a75e36c51a56aa34bf60b51
Jane <janezeng45@gmail.com>
2023-11-10 12:48:58 -0500

    update

--- octoprotect/app/index.js
+++ octoprotect/app/index.js
@@ -1,11 +1,21 @@
-import {View, Text} from 'react-native';
+import { StyleSheet, View, Text, Button } from "react-native";
+// import { Title } from "react-native-paper";
 
 const Home = () => {
-    return (
-        <View>
-            <Text>Hello World</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text>Hello World</Text>
+      <Button title="Click me!" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default Home;
