251d438a851251be405deb7bb2cec7ef6b2bd324
Jane <janezeng45@gmail.com>
2023-11-21 13:11:57 -0500

    Add a Device button is created

---
+++ octoprotect/elements/AddNewDeviceButton.js
@@ -0,0 +1,44 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+const onPress = () => {
+  console.log("Pressed the AddNewDevice");
+};
+
+const AddNewDeviceButton = () => {
+  return (
+    <View style={styles.container}>
+      <Pressable
+        onPress={onPress}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? "#549B0D" : "#5EBC00",
+            alignItems: "center",
+          },
+          styles.wrapperCustom,
+        ]}
+      >
+        <Text style={styles.text}>Add a Device</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignContent: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 19,
+    fontWeight: "500",
+    color: "white",
+  },
+  wrapperCustom: {
+    borderRadius: 20,
+    padding: 12,
+    width: 200,
+  },
+});
+
+export default AddNewDeviceButton;
