f9533bc79ecb9c338399efe3bafeb035af8abdda
Julia Ilioukhina <jilioukhina@uwaterloo.ca>
2023-11-27 21:10:36 -0500

    Update 2 files
    - /octoprotect/app/AlertOverlay.js
    - /octoprotect/store/deviceListSlice.js

--- octoprotect/store/deviceListSlice.js
+++ octoprotect/store/deviceListSlice.js
@@ -1,15 +1,21 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { deviceListAction } from "../service/websocket";
-
+import { movementTriggerAction} from "../service/websocket";
 
 export const deviceListSlice = createSlice({
   name: 'deviceList',
   initialState: {
-    devices: []
+    devices: [],
+    showAlert: false,
   },
   extraReducers: (builder) => {
-    builder.addCase(deviceListAction, (state, data) => {
-      state.devices = data.payload.data
-    })
+    builder
+      .addCase(deviceListAction, (state, data) => {
+        state.devices = data.payload.data;
+      })
+      .addCase(movementTriggerAction, (state) => {
+        state.showAlert = true;
+      });
   },
 })
+export const { showAlert} = deviceListSlice.actions;
